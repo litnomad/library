9b57aa4258e155a70b2986cd9933a410ad19a6d5
Lenna <128262375+waxingrose@users.noreply.github.com>
2025-09-02 17:12:50 -0400

    add delete button to books. add ID to delete buttons and associate with book objects

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const body = document.querySelector('body');
 const form = document.querySelector('fieldset');
 
@@ -14,11 +16,21 @@ document.getElementById('btn').addEventListener('click', () => {
 }
 );
 
+let content;
+let button;
+let div;
+
 document.getElementById('new').addEventListener('click', () => {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
     const status = document.querySelector('input[name="status"]:checked').value;
+
+    content = document.getElementById('content');
+    button = document.createElement('button');
+    div = document.createElement('div');
+
+    const myLibrary = [];
 
     function Book() {
         this.title = title;
@@ -26,33 +38,48 @@ document.getElementById('new').addEventListener('click', () => {
         this.pages = pages;
         this.status = status;
         this.info = function () {
-            return '<b>' + this.title + '</b>' + '<br>' + 'by ' + this.author + '<br>' + this.pages + " pages" + '<br>' + this.status
+            return '<b>' + this.title + '</b>' + '<br>' + this.author + '<br>' + this.pages + " pages" + '<br>' + this.status
         };
     };
-
-    // Write a function that loops through the array and displays each book on the page.
-    const myLibrary = [];
 
     if (title.length > 0 && author.length > 0 && pages.length > 0) {
 
         const newBook = new Book(title, author, pages, status);
         myLibrary.push(newBook.info());
-        console.log(myLibrary);
 
         myLibrary.forEach(myFunction)
         function myFunction(value, index, array) {
-            const content = document.getElementById('content');
-            const div = document.createElement('div');
-            div.setAttribute('id', 'card');
             content.appendChild(div);
+            div.setAttribute('class', 'card');
             div.innerHTML = value;
+            div.setAttribute('id', newBook);
 
             // Add a button on each book’s display to remove the book from the library.
+            div.appendChild(button);
+            button.textContent = 'delete';
+            button.setAttribute('id', newBook);
+            button.setAttribute('onclick', 'deleteBook()');
         }
+
+        /*for (let i = 1; i <= myLibrary.length; i++){
+            content.appendChild(div);
+            div.setAttribute('class', 'card');
+            div.innerHTML = newBook.info();
+            div.setAttribute('id', [i]);
+            div.appendChild(button);
+            button.textContent = 'delete';
+            button.setAttribute('id', [i]);
+            button.setAttribute('onclick', 'deleteBook()');
+        };*/
 
     };
 
 
 });
 
+function deleteBook() {
+    if (div.id === button.id) {
+        div.remove();
+    }
+}
 
