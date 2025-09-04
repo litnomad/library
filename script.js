9d5f3d7735c4225a7d7acdbcc4c6c984a71862bc
Lenna <128262375+waxingrose@users.noreply.github.com>
2025-09-04 16:56:07 -0400

    reorganize myLibrary array to include all books. add remove onclick function to books

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-
-
 const body = document.querySelector('body');
 const form = document.querySelector('fieldset');
-
 
 form.style.visibility = 'hidden';
 
@@ -19,18 +16,16 @@ document.getElementById('btn').addEventListener('click', () => {
 let content;
 let button;
 let div;
+const myLibrary = [];
 
-document.getElementById('new').addEventListener('click', () => {
+document.getElementById('add').addEventListener('click', () => {
+    content = document.getElementById('content');
+    button = document.createElement('button');
+    div = document.createElement('div');
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
     const status = document.querySelector('input[name="status"]:checked').value;
-
-    content = document.getElementById('content');
-    button = document.createElement('button');
-    div = document.createElement('div');
-
-    const myLibrary = [];
 
     function Book() {
         this.title = title;
@@ -43,43 +38,27 @@ document.getElementById('new').addEventListener('click', () => {
     };
 
     if (title.length > 0 && author.length > 0 && pages.length > 0) {
-
         const newBook = new Book(title, author, pages, status);
         myLibrary.push(newBook.info());
 
-        myLibrary.forEach(myFunction)
-        function myFunction(value, index, array) {
-            content.appendChild(div);
-            div.setAttribute('class', 'card');
-            div.innerHTML = value;
-            div.setAttribute('id', newBook);
+        content.appendChild(div);
+        div.setAttribute('id', 'card');
+        div.innerHTML = newBook.info();
 
-            // Add a button on each book’s display to remove the book from the library.
-            div.appendChild(button);
-            button.textContent = 'delete';
-            button.setAttribute('id', newBook);
-            button.setAttribute('onclick', 'deleteBook()');
-        }
-
-        /*for (let i = 1; i <= myLibrary.length; i++){
-            content.appendChild(div);
-            div.setAttribute('class', 'card');
-            div.innerHTML = newBook.info();
-            div.setAttribute('id', [i]);
-            div.appendChild(button);
-            button.textContent = 'delete';
-            button.setAttribute('id', [i]);
-            button.setAttribute('onclick', 'deleteBook()');
-        };*/
+        div.appendChild(button);
+        button.setAttribute('id', 'delete');
+        button.setAttribute('onclick', 'remove(this)');
+        button.textContent = 'delete';
 
     };
 
+    for (let i = 0; i < myLibrary.length; i++) {
+        div.setAttribute('class', [i]);
+        button.setAttribute('class', [i]);
+    }
 
 });
 
-function deleteBook() {
-    if (div.id === button.id) {
-        div.remove();
-    }
+function remove(e) {
+    e.parentElement.remove();
 }
-
